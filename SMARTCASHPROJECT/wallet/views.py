9be81b98693fedbae9b833e3612c231e882ac54f
somendietam <somema501@gmail.com>
2023-02-18 17:13:53 -0500

    Inicio de sesion y registro de usuarios

--- SMARTCASHPROJECT/wallet/views.py
+++ SMARTCASHPROJECT/wallet/views.py
@@ -1,10 +1,26 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-
+from django.shortcuts import render, redirect
+from .forms import CustomUserCreationForm
+from django.contrib.auth import authenticate, login
+from django.contrib import messages
 
 def home(request):
-    return HttpResponse('<h1>Welcome to Home Page</h1>')
+    return render(request, 'home.html')
 
+def registro(request):
+    data = {
+        'form': CustomUserCreationForm()
+    }
+    
+    if request.method == 'POST':
+        formulario = CustomUserCreationForm(data = request.POST)
+        if formulario.is_valid():
+            formulario.save
+            user = authenticate(username = formulario.cleaned_data["username"], password = formulario.cleaned_data["password1"])
+            login(request, user)
+            messages.success(request, "Te has registrado correctamente")
+            
+            return redirect(to = "home") #redirigir al home
+        data["form"] = formulario
+        
+    return render(request, 'registration/registro.html', data)
 
-def about(request):
-    return HttpResponse('<h1>Welcome to About Page!!!</h1>')
